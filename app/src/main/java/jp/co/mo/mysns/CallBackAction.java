df19231fe84bbb8411f5d5ad4282b961d7c32242
Ogai Masataka <gaogaomango@gmail.com>
2018-12-12 01:44:23 +1100

    change gitignore and clear cache

--- app/src/main/java/jp/co/mo/mysns/CallBackAction.java
+++
@@ -1,6 +0,0 @@
-package jp.co.mo.mysns;
-
-public interface CallBackAction {
-    public void onSuccess();
-    public void onFailed();
-}
--- app/src/main/java/jp/co/mo/mysns/Parameter.java
+++
@@ -1,7 +0,0 @@
-package jp.co.mo.mysns;
-
-public class Parameter {
-
-    public static final String PREF_KEY_USER_ID = "user_id";
-
-}
