8f37c85eaa51c26071f440e504b3d7162a2e0685
Ogai Masataka <gaogaomango@gmail.com>
2018-12-11 17:25:21 +1100

    tmp server for add user in php

---
+++ etc/DBScript.sql
@@ -0,0 +1,41 @@
+drop database IF EXISTS twitter;
+create database twitter;
+use twitter;
+
+create table login (
+user_id int AUTO_INCREMENT PRIMARY KEY,
+first_name varchar(250),
+email varchar(250),
+password varchar(250),
+picture_path varchar(250)
+);
+
+describe login;
+
+create table tweets(
+  tweet_id int AUTO_INCREMENT PRIMARY KEY,
+  user_id int,
+  tweet_text varchar(255),
+  tweet_picture varchar(350),
+  tweet_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (user_id) REFERENCES login(user_id)
+);
+
+describe tweets;
+
+create table following(
+  user_id int,
+  following_user_id int,
+  FOREIGN KEY (user_id) REFERENCES login(user_id),
+  FOREIGN KEY (following_user_id) REFERENCES login(user_id)
+);
+
+describe following;
+
+create view user_tweets as
+  select tweets.tweet_id, tweets.tweet_text, tweets.tweet_picture, tweets.tweet_date, login.first_name, login.picture_path
+  from tweets
+  inner join login
+  on tweets.user_id = login.user_id;
+
+describe user_tweets;
---
+++ etc/Login.php
@@ -0,0 +1,31 @@
+<?php
+  require("DBInfo.inc");
+
+// http://0.0.0.0:8080/Login.php?email=masa@gmail.com&password=1234
+
+  $query = "select * from login where email ='". $_GET['email'] ."' and password='". $_GET['password'] ."';";
+
+  $result = mysqli_query($connect, $query);
+  // var_dump($query);
+  // var_dump($result);
+
+  if(!result) {
+    die('Error cannot run query');
+  }
+
+  $userInfo = array();
+  while($row=mysqli_fetch_assoc($result)) {
+    $userInfo[] = $row;
+    break; // to be save;
+  }
+
+  if($userInfo) {
+    print("{'msg':'pass login', 'info':'". json_encode($userInfo) ."'}");
+  } else {
+    print("{'msg':'cannot login'}:'");
+  }
+
+mysqli_free_result($result);
+mysqli_close($connect);
+
+?>
