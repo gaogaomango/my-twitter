3b16d8b2b8bab71b86bd64a90f742c44d0331f19
Ogai Masataka <gaogaomango@gmail.com>
2018-12-11 18:58:10 +1100

    create menu on main

--- app/src/main/java/jp/co/mo/mysns/MainActivity.java
+++ app/src/main/java/jp/co/mo/mysns/MainActivity.java
@@ -2,6 +2,11 @@ package jp.co.mo.mysns;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.home:
+                return true;
+            case R.id.searchBar:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
